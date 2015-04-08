1b12ce21cdc0285633772d4571a9a98e25b3076e
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2015-04-08 15:38:04 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DdnextM2::Application.config.secret_key_base = 'e8431e1d40095765deb48e35285a9b8cd3034089fcdcd3278294d5617f27e97e200cd0c8ef4a5c37c5aa463b9c4d1b59580be611d3576fd0dfa6cf032fba3ae8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DdnextM2::Application.config.session_store :cookie_store, key: '_ddnext-m2_session'
